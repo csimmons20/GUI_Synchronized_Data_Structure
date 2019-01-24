0d6127ab4493925367e0e587044a6f8d2c29a326
alanli <alli19@scienceleadership.org>
2019-01-24 09:43:52 -0500

    Changed the main class.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,9 +8,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Controller newController;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("PictureViewer.fxml"));
+        Parent root = loader.load();
+        newController = loader.getController();
+        newController.setStage(primaryStage);
+
+        Thread.currentThread().setName("PictureView GUI Thread");
+        // Display the scene
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
