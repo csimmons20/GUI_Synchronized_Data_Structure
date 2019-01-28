8d09732b1db2a6b0ce9b479b18459346f9542338
alanli <alli19@scienceleadership.org>
2019-01-28 14:28:31 -0500

    Changed the main class.

--- src/sample/UpdateGUI.java
+++ src/sample/UpdateGUI.java
@@ -1,5 +1,32 @@
 package sample;
 
-public class UpdateGUI {
+
+import javafx.scene.control.TextField;
+
+public class UpdateGUI implements Runnable {
+
     int allData;
+    private SynchronizedQueue originalQueue;
+    private TextField GUIMessageView;
+
+    UpdateGUI(SynchronizedQueue queue, TextField GUIMessage) {
+        originalQueue = queue;
+        GUIMessageView = GUIMessage;
+    }
+
+    public void run() {
+        Thread.currentThread().setName("GUIUpdater Thread");
+
+        while (!Thread.interrupted()) {
+            // Ask queue for a message to open
+            TextField next = (TextField)originalQueue.get();
+            while (next == null) {
+                Thread.currentThread().yield();
+                next = (TextField) originalQueue.get();
+            }
+
+            // FINALLY I have a file to do something with
+            GUIMessageView.setTextField(next);
+        }
+    }
 }
