7c0544ec288a06a3b195ec3e07c349430c190012
alanli <alli19@scienceleadership.org>
2019-02-06 08:41:24 -0500

    Fixed messages.

--- src/sample/UpdateGUI.java
+++ src/sample/UpdateGUI.java
@@ -1,28 +1,31 @@
 package sample;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class UpdateGUI implements Runnable {
 
-    int allData;
-
     private SynchronizedQueue originalQueue;
     private TextField GUIMessageView;
     private ImageView GUIimageView;
+    private ListView TheChat;
 
-    UpdateGUI(SynchronizedQueue queue, TextField GUIMessage, ImageView imageView) {
+
+    UpdateGUI(SynchronizedQueue queue, TextField GUIMessage, ImageView imageView, ListView chat ) {
         originalQueue = queue;
         GUIMessageView = GUIMessage;
         GUIimageView = imageView;
+        TheChat = chat;
     }
 
     public void run() {
         Thread.currentThread().setName("GUIUpdater Thread");
 
         while (!Thread.interrupted()) {
-            // Ask queue for a message to open
+            // Ask queue for a image to display
             Image next = (Image)originalQueue.get();
 
             while (next == null) {
@@ -30,8 +33,24 @@ public class UpdateGUI implements Runnable {
                 next = (Image) originalQueue.get();
             }
 
+            System.out.println("UpdateGUI GOT: " +next);
             // FINALLY I have a file to do something with
             GUIimageView.setImage(next);
+
+            // Ask queue for a message to add to chat
+            String message = (String)originalQueue.get();
+
+            while (message == null) {
+                Thread.currentThread().yield();
+                message = (String) originalQueue.get();
+            }
+
+            System.out.println("UpdateGUI GOT: " +message);
+            // FINALLY I have a file to do something with
+
+            // Update the list view with the text from the bottom text field
+            TheChat.getItems().add(new Label(message));
+            // Clear the bottom text field because it has been used.
 
 
         }
