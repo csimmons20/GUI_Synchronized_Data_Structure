868ef888d160d423f8e60fc284b17250a162c4bb
csimmons20 <csimmons20@scienceleadership.org>
2019-02-15 13:20:01 -0500

    Working on Program - MediaView

--- src/sample/WebChatController.java
+++ src/sample/WebChatController.java
@@ -20,7 +20,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.ArrayList;
+
 
 public class WebChatController {
 
