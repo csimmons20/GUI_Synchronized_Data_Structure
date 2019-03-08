add38caa7a3d277455afd46954b02930be038ba5
csimmons20 <csimmons20@scienceleadership.org>
2019-03-08 13:33:10 -0500

    Sending Media

--- src/sample/Message.java
+++ src/sample/Message.java
@@ -17,8 +17,8 @@ public class Message implements Serializable {
     // Image is transient means that we have to provide our own code to read/write object
     private String data1;
     private transient Image data2;
-    private byte[] data3;
-    private File mediaFile;
+    private transient byte[] data3;
+    private transient File mediaFile;
 
     Message(String who, String text, Image image, File file) {
         sender = who;
@@ -61,8 +61,6 @@ public class Message implements Serializable {
     private void readObject(ObjectInputStream inStream) throws IOException, ClassNotFoundException {
         // this reads sender String with default code
         inStream.defaultReadObject();
-        // this reads data Image using this custom code
-        data2 = SwingFXUtils.toFXImage(ImageIO.read(inStream), null);
 
         DataInputStream dIn = new DataInputStream(inStream);
         int length = dIn.readInt();                    // read length of incoming files
@@ -75,25 +73,30 @@ public class Message implements Serializable {
         FileOutputStream fos = new FileOutputStream(mediaFile);
         fos.write(data3);
         fos.close();
+
+        // this reads data Image using this custom code
+        data2 = SwingFXUtils.toFXImage(ImageIO.read(inStream), null);
     }
 
 
     private void writeObject(ObjectOutputStream outStream) throws IOException {
         // this writes sender String with default code
         outStream.defaultWriteObject();
-        // this writes data Image using this custom code
-        ImageIO.write(SwingFXUtils.fromFXImage(data2, null), "png", outStream);
         // this writes File bytes using custom code
         DataOutputStream dOut = new DataOutputStream(outStream);
         if (data3 == null) {
+            System.out.println("writeObject data3 length = 0");
             dOut.writeInt(0); // write length of the file
         } else {
+            System.out.println("writeObject data3 length = "+ data3.length);
             dOut.writeInt(data3.length); // write length of the file
             if (data3.length > 0) {
                 dOut.write(data3);           // write the file bytes
             }
         }
-        dOut.close();
+        // this writes data Image using this custom code
+        ImageIO.write(SwingFXUtils.fromFXImage(data2, null), "png", outStream);
+
     }
 
 }
